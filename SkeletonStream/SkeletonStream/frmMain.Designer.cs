b38e9d2bb2ffe695ca82c21c33a4b02f196eb0f0
teddyort <teddy@mit.edu>
2016-02-24 22:53:19 -0500

    Adds basic connect and display stream functionality

--- SkeletonStream/SkeletonStream/frmMain.Designer.cs
+++ SkeletonStream/SkeletonStream/frmMain.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.video = new System.Windows.Forms.PictureBox();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.video)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // video
+            // 
+            this.video.Location = new System.Drawing.Point(12, 12);
+            this.video.Name = "video";
+            this.video.Size = new System.Drawing.Size(508, 413);
+            this.video.TabIndex = 0;
+            this.video.TabStop = false;
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(12, 431);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.Size = new System.Drawing.Size(508, 73);
+            this.rtbMessages.TabIndex = 1;
+            this.rtbMessages.Text = "";
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(532, 516);
+            this.Controls.Add(this.rtbMessages);
+            this.Controls.Add(this.video);
+            this.Name = "frmMain";
             this.Text = "Skeleton Stream";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.video)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox video;
+        private System.Windows.Forms.RichTextBox rtbMessages;
     }
 }
 
